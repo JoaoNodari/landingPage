1e62b29bb499d6a53334935609d6903202d55ef5
João Nodari <joaonodari2017@hotmail.com>
2024-12-10 12:08:46 -0300

    Finalizando Projeto.

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,52 @@ const setaAvanca = document.getElementById('btn-avancar')
 const setaVoltar = document.getElementById('btn-voltar')
 let imagemAtual = 0
 
-setaAvanca.addEventListener("click", function() {
-    imagemAtual++
-
+function esconderImagens() {
     imagens.forEach(imagem => {
         imagem.classList.remove('mostrar')
     })
+}
 
+function mostrarImagens(){
     imagens[imagemAtual].classList.add('mostrar')
-    
+}
 
+setaAvanca.addEventListener("click", function() {
+
+    // Testa o contador da imagem se é igual o total das imagens
+
+    const totalDeImagens = imagens.length - 1
+
+    if (imagemAtual >= 0) {
+        setaVoltar.classList.remove('esconder')
+    } if (imagemAtual === totalDeImagens - 1) {
+        setaAvanca.classList.add('esconder')
+    }
+
+    if(imagemAtual === totalDeImagens) {
+        return
+    }
+
+    imagemAtual++
+
+    esconderImagens()
+    mostrarImagens()
+})
+
+setaVoltar.addEventListener("click", function() {
+
+    if (imagemAtual <= 2) {
+        setaAvanca.classList.remove('esconder')
+    } if (imagemAtual === 1) {
+        setaVoltar.classList.add('esconder')
+    }
+
+    if (imagemAtual === 0) {
+        return
+    }
+
+    imagemAtual--
+
+    esconderImagens()
+    mostrarImagens()
 })
